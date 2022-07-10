ea6a8fbc1dc3064592b33812559a949e6d4f4810
Lucas Santos <lucassq_@hotmail.com>
2022-07-09 22:32:10 -0300

    modified comment

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,6 @@ import chess.ChessMatch;
 public class Program {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());//esse metodo vai receber a matriz de peças da partida
+        UI.printBoard(chessMatch.getPieces());//esse metodo vai receber a matriz de peças da partida;
     }
 }
